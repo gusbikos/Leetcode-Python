297963edd6f74e7b20b328c520b0e61b9211945a
Gus Bikos <gusbikos@gmail.com>
2026-01-30 23:42:32 -0600

    first commit

---
+++ Arrays/Easy/twoSum.py
@@ -0,0 +1,9 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        hash_map = {}
+        
+
+        
+sol = Solution()
+result = sol([2,7,11,15], 9)
+print(result)
